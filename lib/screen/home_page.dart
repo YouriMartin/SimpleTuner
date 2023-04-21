f1eaef763f347db81fa3109669bd986f768d568b
ymartin2021 <youri.martin@gmail.com>
2023-04-21 11:43:42 +0200

    start

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,100 @@
+import 'dart:core';
+
+import 'dart:developer' as developer;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fft/flutter_fft.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double? frequency;
+  String? note;
+  int? octave;
+  bool? isRecording;
+
+  FlutterFft flutterFft = FlutterFft();
+
+  _initialize() async {
+    print("Starting recorder...");
+     print("Before");
+     bool hasPermission = await flutterFft.checkPermission();
+     print("After: " + hasPermission.toString());
+
+    // Keep asking for mic permission until accepted
+    while (!(await flutterFft.checkPermission())) {
+      flutterFft.requestPermission();
+      // IF DENY QUIT PROGRAM
+    }
+
+    // await flutterFft.checkPermissions();
+    await flutterFft.startRecorder();
+    print("Recorder started...");
+    setState(() => isRecording = flutterFft.getIsRecording);
+
+    flutterFft.onRecorderStateChanged.listen(
+            (data) => {
+          print("Changed state, received: $data"),
+          setState(
+                () => {
+              frequency = data[1] as double,
+              note = data[2] as String,
+              octave = data[5] as int,
+            },
+          ),
+          flutterFft.setNote = note!,
+          flutterFft.setFrequency = frequency!,
+          flutterFft.setOctave = octave!,
+          print("Octave: ${octave!.toString()}")
+        },
+        onError: (err) {
+          print("Error: $err");
+        },
+        onDone: () => {print("Isdone")});
+  }
+
+  @override
+  void initState() {
+    isRecording = flutterFft.getIsRecording;
+    frequency = flutterFft.getFrequency;
+    note = flutterFft.getNote;
+    octave = flutterFft.getOctave;
+    super.initState();
+    _initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Simple flutter fft example",
+        theme: ThemeData.dark(),
+        color: Colors.blue,
+        home: Scaffold(
+          backgroundColor: Colors.purple,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                isRecording!
+                    ? Text("Current note: ${note!},${octave!.toString()}",
+                    style: TextStyle(fontSize: 30))
+                    : Text("Not Recording", style: TextStyle(fontSize: 35)),
+                isRecording!
+                    ? Text(
+                    "Current frequency: ${frequency!.toStringAsFixed(2)}",
+                    style: TextStyle(fontSize: 30))
+                    : Text("Not Recording", style: TextStyle(fontSize: 35))
+              ],
+            ),
+          ),
+        ));
+  }
+}
